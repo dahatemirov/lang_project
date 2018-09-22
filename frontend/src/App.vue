04b6b598ea629a33374da21d1a8dec68669835f3
Dakhir Temirov <dahatemirov@yandex.ru>
2018-09-23 00:14:59 +0300

    add: vue base from habr tutorial

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,60 +1,118 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="app">
+        <header>
+            <h1>Vue.js SPA</h1>
+        </header>
+        <main>
+            <aside class="sidebar">
+                <router-link
+                        v-for="post in posts"
+                        active-class="is-active"
+                        class="link"
+                        :to="{ name: 'post', params: { id: post.id } }">
+                    {{post.id}}. {{post.title}}
+                </router-link>
+            </aside>
+            <div class="content">
+                <router-view></router-view>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    import axios from 'axios'
+    export default {
+        data () {
+            return {
+                posts: [],
+                endpoint: 'https://jsonplaceholder.typicode.com/posts/',
+            }
+        },
+        created() {
+            this.getAllPosts();
+        },
+        methods: {
+            getAllPosts() {
+                axios.get(this.endpoint)
+                    .then(response => {
+                        this.posts = response.data;
+                    })
+                    .catch(error => {
+                        console.log('-----error-------');
+                        console.log(error);
+                    })
+            }
+        }
     }
-  }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+    body {
+        margin: 0;
+        padding: 0;
+    }
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+    }
+    h1, h2 {
+        font-weight: normal;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+    header {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        min-height: 90px;
+        border-bottom: 1px solid #42b983;
+        text-align: center;
+        background: #ffffff;
+    }
+    main {
+        display: flex;
+        height: calc(100vh - 90px);
+        max-width: 1200px;
+        margin-top: 90px;
+        margin-left: auto;
+        margin-right: auto;
+        overflow: hidden;
+    }
+    aside {
+        flex: 1 0 30%;
+        height: 100%;
+        overflow-y: auto;
+        width: 30%;
+        padding: 50px 30px;
+        box-sizing: border-box;
+        border-right: 1px solid #42b983;
+    }
+    .content {
+        flex: 1 1 70%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .link {
+        display: block;
+        text-decoration: none;
+        margin-bottom: 10px;
+        color: #2c3e50;
+        &--home {
+            text-transform: uppercase;
+            margin-bottom: 30px;
+        }
+        &.is-active {
+            color: #42b983;
+        }
+    }
 </style>
